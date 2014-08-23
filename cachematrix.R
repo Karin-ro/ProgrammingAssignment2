591487abdddad15485ae588bd925a0d180d6a548
Karin <karin_roettgers@hotmail.com>
2014-08-23 22:54:26 +0200

    Main comment insert

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Creates a special "matrix" object that can cache its inverse.
 
 ## The function "makeCacheMatrix" calculates the inverse of a matrix x.
 
