69c5a8a58775b0c432e6ede264513cdce5f4e709
Karin <karin_roettgers@hotmail.com>
2014-08-23 22:37:43 +0200

    Funktion makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,19 @@
 
 ## The function "makeCacheMatrix" calculates the inverse of a matrix x.
 
+makeCacheMatrix <- function(x = matrix()) {
+      i <- NULL
+      set <- function(y) {
+            x <<- y
+            i <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(inv) i <<- inv
+      getinverse <- function() i
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+}
 
 
 makeCacheMatrix <- function(x = matrix()) {
